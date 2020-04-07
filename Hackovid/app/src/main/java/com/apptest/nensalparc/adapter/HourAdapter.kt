68bafb9ca256efde91ac437fec3a8f7408074523
IgnasiPelayo <ignasi.pelayo@enti.cat>
2020-04-07 17:12:49 +0200

    botons amb capacitat

--- Hackovid/app/src/main/java/com/apptest/nensalparc/adapter/HourAdapter.kt
+++ Hackovid/app/src/main/java/com/apptest/nensalparc/adapter/HourAdapter.kt
@@ -1,13 +1,16 @@
 package com.apptest.nensalparc.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 import com.apptest.nensalparc.HourModel
 import com.apptest.nensalparc.R
 import kotlinx.android.synthetic.main.item_timefraction_background.view.*
+import kotlinx.android.synthetic.main.item_timefraction_button.view.*
 import java.sql.Timestamp
 
 class HourAdapter : RecyclerView.Adapter<HourAdapter.ViewHolder>(){
@@ -28,8 +31,19 @@ class HourAdapter : RecyclerView.Adapter<HourAdapter.ViewHolder>(){
 
         holder.hour.text = element.start.toString() + "h"
 
+        Log.i("FRACTION", element.timeFractions?.count().toString())
+
+        holder.background.removeAllViews()
+
+        var i = 1020
         element.timeFractions?.forEach {
-            val timeFraction = LayoutInflater.from(holder.background.context).inflate(R.layout.item_timefraction_button, holder.background, false)
+            var inflatedLayout = LayoutInflater.from(holder.background.context).inflate(R.layout.item_timefraction_button, holder.background, true)
+            inflatedLayout.button_timefraction.text = it.currentCapacity.toString() + "/" + it.maxCapacity.toString()
+            inflatedLayout.button_timefraction.setOnClickListener(){
+
+            }
+            inflatedLayout.button_timefraction.id = i
+            i++
         }
     }
 
--- Hackovid/app/src/main/java/com/apptest/nensalparc/ui/send/SendFragment.kt
+++ Hackovid/app/src/main/java/com/apptest/nensalparc/ui/send/SendFragment.kt
@@ -15,6 +15,7 @@ import com.apptest.nensalparc.HourModel
 import com.apptest.nensalparc.R
 import com.apptest.nensalparc.TimeFractionModel
 import com.apptest.nensalparc.adapter.HourAdapter
+import kotlinx.android.synthetic.main.fragment_reservation.view.*
 import kotlinx.android.synthetic.main.fragment_send.*
 import kotlinx.coroutines.launch
 import java.io.IOException
@@ -29,7 +30,7 @@ class SendFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View?{
-        return inflater.inflate(R.layout.fragment_send, container, false)
+        return inflater.inflate(R.layout.fragment_reservation, container, false)
     }
 
     private val adapter = HourAdapter()
@@ -39,24 +40,32 @@ class SendFragment : Fragment() {
 
         lifecycleScope.launch{
             try {
-
                 recyclerview.adapter = adapter
                 recyclerview.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
 
                 val hours = ArrayList<HourModel>()
-                hours.add(HourModel(1, ArrayList<TimeFractionModel>()))
+                hours.add(HourModel(9, ArrayList<TimeFractionModel>()))
                 hours[0].timeFractions?.add(TimeFractionModel(0, 15, 10, 2))
-                hours[0].timeFractions?.add(TimeFractionModel(15, 15, 10, 2))
+                hours[0].timeFractions?.add(TimeFractionModel(15, 15, 8, 5))
                 hours[0].timeFractions?.add(TimeFractionModel(30, 15, 10, 2))
-                hours[0].timeFractions?.add(TimeFractionModel(45, 15, 10, 2))
+                hours[0].timeFractions?.add(TimeFractionModel(45, 15, 6, 4))
 
-                hours.add(HourModel(2, ArrayList<TimeFractionModel>()))
+                hours.add(HourModel(10, ArrayList<TimeFractionModel>()))
                 hours[1].timeFractions?.add(TimeFractionModel(0, 15, 10, 2))
                 hours[1].timeFractions?.add(TimeFractionModel(15, 15, 10, 2))
                 hours[1].timeFractions?.add(TimeFractionModel(30, 15, 10, 2))
                 hours[1].timeFractions?.add(TimeFractionModel(45, 15, 10, 2))
 
-                // TODO: Set to StreamsAdapter
+                hours.add(HourModel(11, ArrayList<TimeFractionModel>()))
+                hours.add(HourModel(12, ArrayList<TimeFractionModel>()))
+                hours.add(HourModel(13, ArrayList<TimeFractionModel>()))
+                hours.add(HourModel(14, ArrayList<TimeFractionModel>()))
+                hours.add(HourModel(15, ArrayList<TimeFractionModel>()))
+                hours.add(HourModel(16, ArrayList<TimeFractionModel>()))
+                hours.add(HourModel(17, ArrayList<TimeFractionModel>()))
+                hours.add(HourModel(18, ArrayList<TimeFractionModel>()))
+                hours.add(HourModel(19, ArrayList<TimeFractionModel>()))
+
                 adapter.elements = hours
                 adapter.notifyDataSetChanged()
 
